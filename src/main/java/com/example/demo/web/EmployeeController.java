3bd27ebebad6dd41f9ad17c893d5a91bdb2072ed
sdnvarmachintala <32308655+sdnvarmachintala@users.noreply.github.com>
2024-11-19 08:44:57 -0800

    Updated EmployeeController.java

--- src/main/java/com/example/demo/web/EmployeeController.java
+++ src/main/java/com/example/demo/web/EmployeeController.java
@@ -3,13 +3,17 @@ package com.example.demo.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
 import com.example.demo.service.EmployeeService;
+import com.example.demo.service.TaxService;
 
 import jakarta.validation.Valid;
 
@@ -20,10 +24,29 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 	
+	@Autowired
+	private TaxService taxService;
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
 	@PostMapping
 	public ResponseEntity<Employee> saveEmployee(@Valid @RequestBody Employee employee){
 		Employee savedEmployee = employeeService.saveEmployeeDetails(employee);
 		return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
 	}
+	
+	@GetMapping("/{id}/tax-deductions")
+    public ResponseEntity<Double> calculateTax(@PathVariable String id) {
+		Employee employee = employeeRepository.findByEmployeeId(id);
+		String employeeId = employee.getEmployeeId();
+        if (employeeId != null) {
+            double tax = taxService.calculateTax(employee.getSalary());
+            return ResponseEntity.ok(tax);
+        }else {
+        	return ResponseEntity.notFound().build();
+        }
+    }
+
 
 }
